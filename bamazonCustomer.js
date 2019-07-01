11938d5d20e06b279f1cd28361d978795d1acb3e
Megan Wu <meganwu1994@gmail.com>
2019-07-01 13:45:05 -0500

    dotenv file connected

---
+++ bamazonCustomer.js
@@ -0,0 +1,26 @@
+var mysql = require("mysql");
+require('dotenv').config();
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: process.env.DB_PASSWORD,
+  database: "bamazon"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  start();
+});
+
