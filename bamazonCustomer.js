691b2b305bff35bee00022ca8bb5303d3ce55476
Megan Wu <meganwu1994@gmail.com>
2019-07-01 15:20:49 -0500

    product table displayed in node

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,8 @@
 var mysql = require("mysql");
 require('dotenv').config();
 var inquirer = require("inquirer");
-
+var columnify = require('columnify');
+var maintable=[];
 // create the connection information for the sql database
 var connection = mysql.createConnection({
   host: "localhost",
@@ -21,6 +22,23 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
-  start();
+  listOfProducts();
 });
 
+
+function listOfProducts() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      for (var i = 0; i < res.length; i++) {
+        // console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+        maintable.push(res[i]);
+      }
+      var columns = columnify(maintable, {
+        columnSplitter: ' | '
+        
+      })
+      console.log(columns);
+
+      console.log("----------------------------------------------------");
+    });
+  }
